ca8a107c84d0e9453913c2c5f6c21da2f906fb2f
Iván Loire <ivan@iloire.com>
2013-02-24 02:34:45 +1100

    Convert identation from tab to spaces.

--- example/simple_push.js
+++ example/simple_push.js
@@ -1,5 +1,5 @@
 var options = {
-	api_key : '#your api here#'
+  api_key : '#your api here#'
 };
 
 //create ducksnode object
@@ -10,12 +10,12 @@ ducksnode.push ('absolute', Math.random()*1000);
 
 //push call with callback
 ducksnode.push ('boxes1', Math.random()*1000, function(err, response_status){
-	if (err){
-		console.error(err);
-	}
-	else{
-		console.log('OK!');
-	}
+  if (err){
+    console.error(err);
+  }
+  else{
+    console.log('OK!');
+  }
 });
 
 //with timestamp (we send object instead of primitive value)
@@ -25,7 +25,7 @@ ducksnode.push ('chart1', {value:111, timestamp: timestamp});
 // push to more than one widget, with callback
 ducksnode.push(['my_widget1', 'my_widget2'], 324, function (err, response_status){
   //this callback will be called once for each widget
-	if (err){
-		console.error(err); //error pushing to ducksboard server.
-	}
+  if (err){
+    console.error(err); //error pushing to ducksboard server.
+  }
 });
